4b9c8c21570cd397d02e586c6e692fc3ce4acbf4
Katarhu <mkaimg1@gmail.com>
2023-02-16 08:45:56 +0200

    (#10) Done

--- src/dish/dto/save-dish.dto.ts
+++ src/dish/dto/save-dish.dto.ts
@@ -6,7 +6,11 @@ export class SaveDishDto {
   readonly dishesToSave: SavedDish[];
 }
 
-const dishTupleJoi = Joi.array().items(Joi.string().required(), Joi.number().positive().min(1).required());
+const dishTupleJoi = Joi.array()
+  .items(
+    Joi.string().required(),
+    Joi.number().positive().min(1).required()
+  );
 
 export const saveDishJoi = Joi.object({
   dishesToSave: Joi.array().required().items(dishTupleJoi),
